356ab8d25eea4470507bc07b4dbb9a214fe3e2b3
ry0mat0 <rmatoike@gmail.com>
2022-01-05 02:26:17 +0900

    feat(*): first commit

---
+++ main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"fmt"
+	"image/color"
+	_ "image/png"
+	"log"
+	"math"
+	"strconv"
+	"strings"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+
+	// "golang.org/x/exp/shiny/screen"
+	"ebiten_hello/player"
+)
+
+const (
+	pi      = math.Pi
+	screenX = 640
+	screenY = 480
+
+	// game modes
+	modeTitle    = 0
+	modeGame     = 1
+	modeGameover = 2
+
+	//player
+	speed       = 1.0
+	accel       = 3.0
+	brake       = 0.5
+	rotSpeed    = 0.02
+	playerSizeX = 100
+	playerSizeY = 100
+)
+
+var gop_img *ebiten.Image
+
+func init() {
+	var err error
+	gop_img, _, err = ebitenutil.NewImageFromFile("image/plane.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Game struct {
+	mode   int
+	dir    int
+	player *player.Player
+}
+
+// NewGame method
+func NewGame() *Game {
+	g := &Game{}
+	g.init()
+	return g
+}
+
+func (g *Game) init() {
+	g.player = &player.Player{}
+	g.player.NewPlayer(playerSizeX, playerSizeY, screenX, screenY, speed, accel, brake, rotSpeed)
+}
+
+func (g *Game) Update() error {
+	switch g.mode {
+	case modeTitle:
+		if g.isKeyJustPressed() {
+			g.mode = modeGame
+			fmt.Println("mode Changed")
+		}
+	case modeGame:
+		g.dir = g.directionKey()
+		g.player.Move(g.dir, screenX, screenY)
+	case modeGameover:
+	}
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.RGBA{0, 0, 0xff, 0xff})
+	switch g.mode {
+	case modeTitle:
+		ebitenutil.DebugPrint(screen, "Title")
+	case modeGame:
+		ebitenutil.DebugPrint(screen, "Game")
+		g.DrawPlayer(screen)
+	case modeGameover:
+		ebitenutil.DebugPrint(screen, "GameOver")
+	}
+}
+
+func (g *Game) DrawPlayer(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Rotate(g.player.Direction)
+	op.GeoM.Translate(g.player.X, g.player.Y)
+	op.GeoM.Scale(1.0, 1.0)
+	s := strconv.FormatFloat(g.player.X, 'f', 1, 64)
+	t := strconv.FormatFloat(g.player.Y, 'f', 1, 64)
+	u := strconv.FormatFloat(g.player.Direction*180.0/pi, 'f', 1, 64)
+	ebitenutil.DebugPrintAt(screen, strings.Join([]string{"X is ", s}, ""), 0, 15)
+	ebitenutil.DebugPrintAt(screen, strings.Join([]string{"Y is ", t}, ""), 0, 30)
+	ebitenutil.DebugPrintAt(screen, strings.Join([]string{"D is ", u}, ""), 0, 45)
+	screen.DrawImage(gop_img, op)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return screenX, screenY
+}
+
+func (g *Game) isKeyJustPressed() bool {
+	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+		return true
+	}
+	return false
+}
+
+func (g *Game) directionKey() int {
+	if ebiten.IsKeyPressed(ebiten.KeyW) {
+		return 1
+	} else if ebiten.IsKeyPressed(ebiten.KeyA) {
+		return 2
+	} else if ebiten.IsKeyPressed(ebiten.KeyS) {
+		return 3
+	} else if ebiten.IsKeyPressed(ebiten.KeyD) {
+		return 4
+	} else {
+		return 0
+	}
+}
+
+func main() {
+	ebiten.SetWindowSize(screenX, screenY)
+	ebiten.SetWindowTitle("EbitenGame")
+	if err := ebiten.RunGame(NewGame()); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ player/player.go
@@ -0,0 +1,98 @@
+package player
+
+import (
+	"fmt"
+	"math"
+)
+
+type Player struct {
+	X         float64 // X posiiton of player
+	Y         float64 // Y position of player
+	Direction float64 // Direction of player
+	r         float64 // Distance between center and lefttop of img
+	thetaP    float64 // Angle of vector between center and lefttop of img
+	speed     float64 // speed of player
+	accel     float64 // accel magnitude of player
+	brake     float64 // brake magnitude of player
+  rot_speed float64 // rotation speed of player
+}
+
+//Initialise player parameters
+func (p *Player) NewPlayer(px, py, sx, sy int, speed, accel, brake, rot_speed float64) {
+	p.X = float64(sx)/2 - float64(px)/2
+	p.Y = float64(sy)/2 - float64(py)/2
+	p.r = math.Sqrt(float64(px)*float64(px)+float64(py)*float64(py)) / 2.0
+	p.thetaP = math.Atan(float64(py) / float64(px))
+	p.speed = speed
+	p.accel = accel
+	p.brake = brake
+	p.rot_speed = rot_speed
+	fmt.Printf("p.r is %f\n", p.r)
+	fmt.Printf("p.thetaP is %f\n", p.thetaP)
+}
+
+func (p *Player) Move(dir, sx, sy int) {
+	p.rotateCenter(dir)
+	p.translate(dir, sx, sy)
+}
+
+func (p *Player) translate(dir, sx, sy int) {
+	switch dir {
+	case 1: //KeyW
+		p.Y = p.Y - p.accel*p.speed*math.Cos(p.Direction)
+		p.X = p.X + p.accel*p.speed*math.Sin(p.Direction)
+	// case 2: //KeyA
+	case 3: //KeyS
+		p.Y = p.Y - p.brake*p.speed*math.Cos(p.Direction)
+		p.X = p.X + p.brake*p.speed*math.Sin(p.Direction)
+	// case 4: //KeyD
+	default:
+		p.Y = p.Y - p.speed*math.Cos(p.Direction)
+		p.X = p.X + p.speed*math.Sin(p.Direction)
+	}
+	p.checkScreenEdge(sx, sy)
+}
+
+// Update angle for rotation
+func (p *Player) rotate(dir int) {
+	switch dir {
+	// case 1: //KeyW
+	case 2: //KeyA
+		p.Direction = p.Direction - p.rot_speed
+	// case 3: //KeyS
+	case 4: //KeyD
+		p.Direction = p.Direction + p.rot_speed
+	}
+}
+
+// Rotate plater around the center of img
+func (p *Player) rotateCenter(dir int) {
+	xc1, yc1 := p.getCenter()
+	p.rotate(dir)
+	xc2, yc2 := p.getCenter()
+	p.X = p.X + (xc1 - xc2)
+	p.Y = p.Y + (yc1 - yc2)
+}
+
+// Detece edge of screen and move to the opposite side
+func (p *Player) checkScreenEdge(sx, sy int) {
+	if p.X < 0 {
+		p.X = p.X + float64(sx)
+	}
+	if p.X > float64(sx) {
+		p.X = p.X - float64(sx)
+	}
+	if p.Y < 0 {
+		p.Y = p.Y + float64(sy)
+	}
+	if p.Y > float64(sy) {
+		p.Y = p.Y - float64(sy)
+	}
+}
+
+//Get center position of player img
+func (p *Player) getCenter() (xc, yc float64) {
+	xc = p.X + p.r*math.Cos(p.Direction+p.thetaP)
+	yc = p.X + p.r*math.Sin(p.Direction+p.thetaP)
+	return
+}
